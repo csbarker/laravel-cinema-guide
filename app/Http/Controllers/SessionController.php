c6c77352118d027598d73ea479d24178d0e6c25e
Callum Barker <callum.scott.barker@gmail.com>
2018-06-30 12:11:48 +0800

    cleaning up - moved inline routes to controllers  Callum Barker committed

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\SessionTime as SessionTime;
+use App\Http\Resources\SessionTimeCollection as SessionTimeCollection;
+
 class SessionController extends Controller
 {
     public function index(Request $request, $cinema_id = 'any', $movie_id = 'all') {
